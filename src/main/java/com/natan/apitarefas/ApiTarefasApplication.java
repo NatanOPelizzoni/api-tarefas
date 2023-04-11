f3a3fc2adb3b6219685f832d7a86287fbdd4e33e
Natan O Pelizzoni <natan2624@gmail.com>
2023-04-11 02:00:40 -0300

    Criado o DTO para a entidade usuario

--- src/main/java/com/natan/apitarefas/ApiTarefasApplication.java
+++ src/main/java/com/natan/apitarefas/ApiTarefasApplication.java
@@ -1,11 +1,23 @@
 package com.natan.apitarefas;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiTarefasApplication {
-
+	
+	/**
+	 * Implementção necessária para utilizar nos controllers de update
+	 * @return
+	 */
+	@Bean
+	public ModelMapper modelMapper(){
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setSkipNullEnabled(true);
+		return modelMapper;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ApiTarefasApplication.class, args);
 	}
---
+++ src/main/java/com/natan/apitarefas/dto/UsuarioDto.java
@@ -0,0 +1,37 @@
+package com.natan.apitarefas.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "tb_usuario")
+public class UsuarioDto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "senha", nullable = false)
+    private String senha;
+
+    @Transient
+    private String token;
+}
